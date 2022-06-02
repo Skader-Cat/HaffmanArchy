53baac1eb3b142253953836baf40d46053e69c0f
Владимир Архипов <65547922+Skader-Cat@users.noreply.github.com>
2022-06-03 01:52:36 +0300

    Удалил лишние функции
    Добавил описание

--- Code.h
+++ Code.h
@@ -8,34 +8,24 @@
 
 using namespace std;
 
-struct elem
+struct elem //структура данных. Пока ещё не конечный её вид
 {
 	string simb;
-	elem* p;
 	elem* l;
 	elem* r;
 	int mark = false;
 	int counter;
 };
 
-string get_path();
-string replace(string path);
+string get_path(); //получает путь
+string replace(string path); //меняет символы, на которые винда ругается
 
 
-bool check_for_void(fstream& file);
-bool check_for_max(list<elem>* tree, int size_of_simbs);
+bool check_for_void(fstream& file); //не реализовано
+fstream get_file(string path); //инициализирует файл и по хорошему тут должны выполняться функции проверок. Но у меня таких нет пока что ха-ха-ха)))
 
-fstream get_file(string path);
-
-list <elem> dict_to_list(map<string, int> dict);
-list <elem> make_sluzheb_info(fstream &file, string path, int* size_of_simbs);
-void show_infos(list<elem> infos);
-
-list <elem> make_new_node(list<elem> infos);
-
-bool check_infos(list<elem> infos, int size_of_simbs);
-
-void make_node(list<elem> *tree, int size_of_simbs);
-
-void sort_elems(list<elem>* tree);
-
+list <elem> dict_to_list(map<string, int> dict); //функция для преобразования из словаря в список. Используется ниже
+list <elem> make_sluzheb_info(fstream &file, string path, int* size_of_simbs); //тут идёт чтение из файла, создание словаря. Тут же он преобразуется в список
+void show_infos(list<elem> tree); //отладочная функция. Рисует дерево 
+void make_node(list<elem> *tree, int size_of_simbs); //создает новый узел. size_of_simbs использовался в прошлом и являлся изначальным кол-вом символов в словаре
+//но потом я нашёл лучший способ!
